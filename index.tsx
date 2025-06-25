5a7e8062935e818ab0f047ebdff11a6e3cb4d12f
Rakesh Singh <rakeshsingh432165@gmail.com>
2025-06-25 18:53:29 +0530

    fix: dotenv not needed

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { createRoot } from 'react-dom/client';
 import { WeatherData } from './types';
-import dotenv from 'dotenv';
 import {
   kelvinToCelsius,
   celsiusToFahrenheit,
